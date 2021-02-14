3153a05998f0fa626cdea9c705d6bad377df09e5
socamacho <socamachoruiz@gmail.com>
2021-02-14 04:09:25 +0000

    Proyecto finalizado

---
+++ index.js
@@ -0,0 +1,9 @@
+function generator() {  
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let msg =who[Math.floor(Math.random() * who.length)] +  " " +action[Math.floor(Math.random() * action.length)] +" " +what[Math.floor(Math.random() * what.length)] + " " +when[Math.floor(Math.random() * when.length)];
+document.getElementById("excuse").innerHTML = msg;
+}   
